2d8271c255e40c9b40068721b816cd92e00a0bd9
JUYOUNG LEE <75352728+leekj3133@users.noreply.github.com>
2021-12-21 20:12:55 +0900

    4th commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.admin.filters import ChoicesFieldListFilter
 
-from .models import Question
+from .models import Choice,Question
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ["question_text"]}),
+        ("Date information",{"fields":["pub_date"]}),
+    ]
+    list_display=("question_text","pub_date", "was_published_recently")
+    
+    inlines=[ChoiceInline]
+    list_filter=["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
