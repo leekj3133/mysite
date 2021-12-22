5781f75348010e868411fdae85534a42030acd32
JUYOUNG LEE <75352728+leekj3133@users.noreply.github.com>
2021-12-22 14:46:06 +0900

    5th commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,14 +18,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+MY_SECRET = {
+    "SECRET_KEY" : "django-insecure-ie^&s%o-o)!ddau7qx83zfqvv(pdu_(ma@f1q_6pcisg@g!uy9"
+}
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ie^&s%o-o)!ddau7qx83zfqvv(pdu_(ma@f1q_6pcisg@g!uy9'
+SECRET_KEY = MY_SECRET['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fore.pythonanywhere.com"]
 
 
 # Application definition
